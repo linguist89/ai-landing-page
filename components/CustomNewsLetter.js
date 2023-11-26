6d778e8b89ffd0f7f9b6bb5421fb7cd38bbabe79
stephan <smuts1989@gmail.com>
2023-11-26 03:16:14 +0100

    Added blackslashes for the apostrophes to be able to deploy on Netlify

--- components/CustomNewsLetter.js
+++ components/CustomNewsLetter.js
@@ -6,7 +6,7 @@ const Newsletter = () => {
     <section className="bg-base-200 border-t  text-center border-base-content/10">
       <div className="max-w-7xl mx-auto px-8 py-24">
         <h2 className="font-bold text-3xl md:text-5xl tracking-tight mb-8 md:mb-12">
-          Don't Forget to Join the Newsletter
+          Don\'t Forget to Join the Newsletter
         </h2>
         <ButtonLead extraStyle="max-w-xl mx-auto" />
       </div>
