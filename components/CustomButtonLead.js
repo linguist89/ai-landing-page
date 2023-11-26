d656d78918e24baf9c51aa95fab044e107102579
stephan <smuts1989@gmail.com>
2023-11-26 03:12:14 +0100

    Fixed some errors for netlify

--- components/CustomButtonLead.js
+++ components/CustomButtonLead.js
@@ -1,38 +1,10 @@
 "use client";
-
-import { useState, useRef } from "react";
-import { toast } from "react-hot-toast";
-import apiClient from "@/libs/api";
-
 // This component is used to collect the emails from the landing page
 // You'd use this if your product isn't ready yet or you want to collect leads
 // For instance: A popup to send a freebie, joining a waitlist, etc.
 // It calls the /api/lead/route.js route and store a Lead document in the database
 const ButtonLead = ({ extraStyle }) => {
-  const inputRef = useRef(null);
-  const [email, setEmail] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e?.preventDefault();
-
-    setIsLoading(true);
-    try {
-      await apiClient.post("/lead", { email });
-
-      toast.success("Thanks for joining the waitlist!");
-
-      // just remove the focus on the input
-      inputRef.current.blur();
-      setEmail("");
-      setIsDisabled(true);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
   return (
     <form
       action="https://spsdigitaltech.us21.list-manage.com/subscribe/post?u=fa5dd95d9df20840e23e99b45&amp;id=5d2ac2a9d4&amp;f_id=000feae6f0"
