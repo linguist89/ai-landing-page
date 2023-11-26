559326680dcde405f65ef6a8e53be5e1ef590356
stephan <smuts1989@gmail.com>
2023-11-26 03:57:08 +0100

    Added correct hotjar id

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
           __html: `
           (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3739630,hjsv:6};
+            h._hjSettings={hjid:3755429,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
